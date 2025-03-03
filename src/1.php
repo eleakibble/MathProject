15a3e1c03dcc508e8a945179bf9eec36f1c5b861
eleakibble <elea_kibble@gmx.com>
2025-03-03 13:41:47 +0000

    Add 1.php

---
+++ src/1.php
@@ -0,0 +1,14 @@
+
+<?php
+
+$x = 5; // Change this value to test different scenarios
+
+if ($x > 3) {
+    echo "The number is greater than 3";
+} else if ($x == 3) {
+    echo "The number is equal to 3";
+} else {
+    echo "The number is less than 3";
+}
+
+?>
